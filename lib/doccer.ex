0a1b8e14a47ea9b75d0fd873f41da53edc6a9f1a
Niek van de Pas <niek_pas@live.nl>
2020-11-25 09:57:10 +0100

    refactor: rename

--- lib/doccer.ex
+++ lib/doccer.ex
@@ -29,31 +29,31 @@ defmodule Doccer do
     end
   end
 
-  defp get_arg_value(args, flag) do
+  defp get_flag_value(args, flag) do
     index = Enum.index(args, fn arg -> arg == flag end)
     if index == nil, do: nil, else: args[index + 1]
   end
 
   defp get_title(args) do
-    get_arg_value(args, "--title")
+    get_flag_value(args, "--title")
   end
 
   defp get_author_name(args) do
-    get_arg_value(args, "--author")
+    get_flag_value(args, "--author")
   end
 
   defp get_year(args) do
-    year_value = get_arg_value(args, "--year")
+    year_value = get_flag_value(args, "--year")
     # TODO validation
     # raise "Invalid year format for year #{year_value}. Years should be 1-4 digits." unless year_value.match?(/[0-9]{,4}/)
   end
 
   defp get_journal_name(args) do
-    get_arg_value(args, "--journal")
+    get_flag_value(args, "--journal")
   end
 
   defp get_tags(args) do
-    get_arg_value(args, "--tags")
+    get_flag_value(args, "--tags")
     |> String.split(",")
     |> Enum.map(fn tag -> String.trim(tag) end)
   end
